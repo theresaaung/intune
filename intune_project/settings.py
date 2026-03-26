9749f2c7b09e78167aacd1020055aa75d7f56cd3
Theresa Aung <3038714w@student.gla.ac.uk>
2026-03-26 18:30:00 +0000

    fix static files settings

--- intune_project/settings.py
+++ intune_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = "django-insecure-n0twuc0ze9*1anehx*@22h8heiw-gne9ayq%h_vjjtaf6wessz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['theresaaung.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['intune.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
